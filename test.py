95458be57cbd2dd196aaa1e6071c59efe5cb6c54
Andrew Pendleton <andrew.d.pendleton@gmail.com>
2020-08-18 22:50:55 -0500

    Nightly updates v0.2" " "

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 import pandas as pd 
 from pytrends.request import TrendReq
-
 pytrend = TrendReq()
 
+# Search specific terms
+pytrend.build_payload(kw_list=['Taylor Swift'])
+# Interest by region
+df = pytrend.interest_by_region()
+df.head(10)
+print (df.head())
+
+# Daily search trends
 df = pytrend.trending_searches(pn='united_states')
 df.head()
+print (df.head())
