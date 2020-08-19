cfad7a83b0aef48730cb1bd06315b4dd21d68d6b
Andrew Pendleton <andrew.d.pendleton@gmail.com>
2020-08-18 23:53:49 -0500

    Updating test.py with connection test for google sheets

--- test.py
+++ test.py
@@ -1,15 +1,19 @@
+import pygsheets
 import pandas as pd 
-from pytrends.request import TrendReq
-pytrend = TrendReq()
+# authorization
+gc = pygsheets.authorize(service_file='key/creds.json')
 
-# Search specific terms
-pytrend.build_payload(kw_list=['Taylor Swift'])
-# Interest by region
-df = pytrend.interest_by_region()
-df.head(10)
-print (df.head())
+# Create empty dataframe
+df = pd.DataFrame()
 
-# Daily search trends
-df = pytrend.trending_searches(pn='united_states')
-df.head()
-print (df.head())
+# Create a column
+df['name'] = ['John', 'Steve', 'Sarah']
+
+# open the google spreadsheet (where test_sheet is the name of my sheet)
+sh = gc.open('test_sheet')
+
+#select the first sheet
+wks = sh[0]
+
+# update the first sheet with df, starting at cell B2.
+wks.set_dataframe(df, (1,1))
