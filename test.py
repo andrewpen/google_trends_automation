19c62604290a27c7ac899bcf2e9ad4cfedae2e40
Andrew Pendleton <andrew.d.pendleton@gmail.com>
2020-08-19 18:26:21 -0500

    Got the connection with Google Sheets working

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+print("success 1")
 import pygsheets
 import pandas as pd 
 # authorization
@@ -9,10 +10,12 @@ df = pd.DataFrame()
 # Create a column
 df['name'] = ['John', 'Steve', 'Sarah']
 
+print(df['name'])
+
 # open the google spreadsheet (where test_sheet is the name of my sheet)
 sh = gc.open('test_sheet')
 
-#select the first sheet
+# select the first sheet
 wks = sh[0]
 
 # update the first sheet with df, starting at cell B2.
